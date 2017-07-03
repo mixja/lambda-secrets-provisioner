c84dceb305e792e3bdbe03f2d0c14b2ed289e7a3
Justin Menga <justin.menga@gmail.com>
2017-07-03 17:41:45 +1200

    Add support for tagging parameters, improve delete logic

--- src/secrets_provisioner.py
+++ src/secrets_provisioner.py
@@ -3,7 +3,7 @@ parent_dir = os.path.abspath(os.path.dirname(__file__))
 vendor_dir = os.path.join(parent_dir, 'vendor')
 sys.path.append(vendor_dir)
 
-import logging, datetime, json, random, string, base64
+import logging, datetime, json, uuid, random, string, base64
 import boto3
 from cfn_lambda_handler import Handler
 from voluptuous import Required, All, Schema, Invalid, MultipleInvalid
@@ -19,7 +19,7 @@ def format_json(data):
   return json.dumps(data, default=lambda d: d.isoformat() if isinstance(d, datetime.datetime) else str(d))
 
 # Lambda handler
-handler = Handler()
+handler = Handler(secure_attributes=['Value'])
 
 # Boto3 clients
 kms = boto3.client('kms')
@@ -31,7 +31,6 @@ def get_validator():
     Required('Name'): All(basestring),
     Required('Key'): All(basestring),
     Required('Value', default=None): All(basestring),
-    Required('Version', default=datetime.datetime.now().isoformat()): All(basestring),
     Required('KmsKeyId'): All(basestring)
   }, extra=True)
 
@@ -48,8 +47,10 @@ def generate_password(length, chars):
 @handler.create
 @handler.update
 def handle_create(event, context):
-  log.info("Received create event: %s" % format_json(event))
+  log.info("Received event: %s" % format_json(event))
   secret = validate(event['ResourceProperties'])
+  # Create UUID for secret
+  secret['Id'] = str(uuid.uuid4())
   if secret['Value'] is None:
     # generate random password
     secret['Value'] = generate_password(LENGTH, CHARS)
@@ -65,11 +66,14 @@ def handle_create(event, context):
     KeyId=secret['KmsKeyId'],
     Overwrite=True
   )
-  event['PhysicalResourceId'] = event['LogicalResourceId']
-  event['Data'] = {
-    'Value': secret['Value'],
-    'Version': secret['Version']
-  }
+  # Tag the parameter with the UUID
+  ssm.add_tags_to_resource(
+    ResourceType='Parameter',
+    ResourceId=secret['Name'],
+    Tags=[{'Key': 'Id', 'Value': secret['Id']}]
+  )
+  event['PhysicalResourceId'] = secret['Id']
+  event['Data'] = {'Value': secret['Value']}
   return event
 
 # Delete requests
@@ -77,5 +81,11 @@ def handle_create(event, context):
 def handle_delete(event, context):
   log.info("Received delete event: %s" % format_json(event))
   secret = validate(event['ResourceProperties'])
-  ssm.delete_parameters(Names=[secret['Name']])
+  # Delete parameter if it exists and is tagged with an Id that matches the physical resource ID
+  if (not [invalid for invalid in ssm.get_parameters(Names=[secret['Name']])['InvalidParameters']] and
+      next((
+        tag for tag in ssm.list_tags_for_resource(ResourceType='Parameter',ResourceId=secret['Name'])['TagList']
+        if tag.get('Key') == ('Id') and tag.get('Value') == event['PhysicalResourceId']
+      ), None)):
+    ssm.delete_parameter(Name=secret['Name'])
   return event
